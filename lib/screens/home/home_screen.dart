3939e11f549f0a23944cf1f32f78ce27ad03f93d
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-09-25 09:48:49 +0200

    add appbar

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../config/constants.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          padding: const EdgeInsets.only(left: defaultPadding),
+          icon: SvgPicture.asset("assets/icons/menu.svg"),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+            icon: SvgPicture.asset("assets/icons/search.svg"),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
