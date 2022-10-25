291d6e13a606860a2f4ebbcf067532c426f9d1fb
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-10-25 23:02:23 +0200

    mack Genre-card reusable

--- lib/components/genres.dart
+++ lib/components/genres.dart
@@ -3,18 +3,12 @@ import 'package:flutter/material.dart';
 import '../config/constants.dart';
 
 class Genres extends StatelessWidget {
-  const Genres({Key key}) : super(key: key);
+  List<String> genres;
+  Genres({this.genres,Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    List<String> genres = [
-      "Action",
-      "Crime",
-      "Comedy",
-      "Drama",
-      "Horror",
-      "Animation"
-    ];
+
     return Container(
       margin: const EdgeInsets.symmetric(vertical: defaultPadding / 2),
       height: 36,
