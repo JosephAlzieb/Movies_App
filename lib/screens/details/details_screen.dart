cf32af0dd1c577df852ee2c24e042fb0ab68bc4b
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-10-25 23:03:26 +0200

    add components to details-screen

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/components/app_bar.dart';
+import 'package:movies_app/components/genres.dart';
 import 'package:movies_app/components/rating.dart';
 import 'package:movies_app/components/title_and_duration.dart';
 import 'package:movies_app/models/movie.dart';
+
+import '../../config/constants.dart';
 
 class DetailsScreen extends StatelessWidget {
   final Movie movie;
@@ -15,7 +18,27 @@ class DetailsScreen extends StatelessWidget {
       body: Column(
         children: [
           Rating(movie: movie,),
-          TitleAndDuration(movie: movie,)
+          TitleAndDuration(movie: movie,),
+          Genres(genres: movie.genra),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: defaultPadding / 2,
+              horizontal: defaultPadding,
+            ),
+            child: Text(
+              "Plot Summary",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+            child: Text(
+              movie.plot,
+              style: const TextStyle(
+                color: Color(0xFF737599),
+              ),
+            ),
+          ),
         ],
       ),
     );
